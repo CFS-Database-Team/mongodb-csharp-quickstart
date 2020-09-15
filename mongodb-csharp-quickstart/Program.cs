552c41ecf3341f40a85da7a7699cbd44a8a8e67a
Hidequel Puga <codefivestar@gmail.com>
2020-09-14 21:13:23 -0500

    Starting and setup

--- mongodb-csharp-quickstart/Program.cs
+++ mongodb-csharp-quickstart/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MongoDB.Driver;
 
 namespace mongodb_csharp_quickstart
 {
@@ -6,7 +7,15 @@ namespace mongodb_csharp_quickstart
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MongoClient dbClient = new MongoClient("mongodb+srv://bounty:UfQaM1vvL33LKw7x@cluster-c5s-001.lxwl3.mongodb.net/sample_mflix?retryWrites=true&w=majority");
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
         }
     }
 }
